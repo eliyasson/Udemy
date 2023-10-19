cc8f398384bb7d791e7e943fe6403107251a970f
Eliyas Kassaye <eliyasson11@gmail.com>
2023-10-19 13:42:35 +0300

    back to js

---
+++ HTML portfolio project/script.js
@@ -0,0 +1,8 @@
+function changeLight(status){
+    const bulb = document.getElementById("bulb");
+    if (status === 'on'){
+      bulb.src = "./assets/images/bulb on.png";
+    } else if (status === 'off') {
+      bulb.src = "./assets/images/bulb off.png";
+    }
+  }
