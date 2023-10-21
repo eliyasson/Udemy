f53b496a1e0c0eb3ce531e3308dc9f9969db5648
Eliyas Kassaye <eliyasson11@gmail.com>
2023-10-21 23:58:56 +0300

    Global Scope and Functions

--- HTML portfolio project/script.js
+++ HTML portfolio project/script.js
@@ -1,3 +1,21 @@
-var tweet = prompt("Compose your tweet: ");
-var tweetCount = tweet.length;
-alert("You have written " + tweetCount + " characters, you have " + (140 - tweetCount) + " characters remaining.")
+// Declare the myGlobal variable below this line
+const myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal here
+  oopsGlobal = 5;
+
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
