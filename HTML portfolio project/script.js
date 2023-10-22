33b1481ebccb7d9c2b94b8f27a7870b0da6b033c
Eliyas Kassaye <eliyasson11@gmail.com>
2023-10-22 14:43:12 +0300

    commit

--- HTML portfolio project/script.js
+++ HTML portfolio project/script.js
@@ -1,21 +1,8 @@
-// Declare the myGlobal variable below this line
-const myGlobal = 10;
-
-function fun1() {
-  // Assign 5 to oopsGlobal here
-  oopsGlobal = 5;
-
-}
-
-// Only change code above this line
-
-function fun2() {
-  let output = "";
-  if (typeof myGlobal != "undefined") {
-    output += "myGlobal: " + myGlobal;
-  }
-  if (typeof oopsGlobal != "undefined") {
-    output += " oopsGlobal: " + oopsGlobal;
-  }
-  console.log(output);
-}
+function changeLight(status){
+      const bulb = document.getElementById("bulb");
+      if (status === 'on'){
+        bulb.src = "./assets/images/bulb on.png";
+      } else if (status === 'off') {
+        bulb.src = "./assets/images/bulb off.png";
+      }
+    }
