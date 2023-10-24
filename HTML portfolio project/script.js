821b8a40d73a4b75b8b0077a97ab7c3eb68b0c15
Eliyas Kassaye <eliyasson11@gmail.com>
2023-10-24 09:58:18 +0300

    output & return value

--- HTML portfolio project/script.js
+++ HTML portfolio project/script.js
@@ -5,4 +5,18 @@ function changeLight(status){
       } else if (status === 'off') {
         bulb.src = "./assets/images/bulb off.png";
       }
-    }
+}
+
+function getMilk(money, price) {   
+    console.log("leave House");
+    console.log("I can buy " + calcBottles(money, price) + " bottles of milk");
+    return money % price;
+}
+
+function calcBottles(money, price) {
+    var numberOfBottle = Math.floor(money / price);
+    return numberOfBottle;
+}
+  getMilk(5, 1.5);
+  var change = getMilk(5, 1.5);
+  console.log(change);
