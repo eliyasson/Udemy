ea2db3dba4606760472cabd2cc8268cda744855a
Eliyas Kassaye <eliyasson11@gmail.com>
2023-10-24 15:45:39 +0300

    if/else

--- HTML portfolio project/script.js
+++ HTML portfolio project/script.js
@@ -1,6 +1,16 @@
-
-
-
+function bmiCalculator(weight, height) {
+    const interpretation = weight / Math.pow(height, 2);
+    if (interpretation < 18.5) {
+        return "Your BMI is " + interpretation + " so you are underweight."
+    }else if (interpretation > 18.5 && interpretation < 24.9) {
+        return "Your BMI is " + interpretation + " so you have a normal weight."
+    } else if(interpretation > 24.9) {
+        return "Your BMI is " + interpretation + " so you are overweight."
+    }
+    return interpretation;
+}
+var bmi = bmiCalculator(100, 2);
+console.log(bmi);
 
 /* 
 //BMI calculator
