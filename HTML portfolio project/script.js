4953f1418d9d6bd3807ea35c72ccc5d7a3ad64db
Eliyas Kassaye <eliyasson11@gmail.com>
2023-11-08 12:02:45 +0200

    high order function

--- HTML portfolio project/script.js
+++ HTML portfolio project/script.js
@@ -1,3 +1,18 @@
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function calculator(num1, num2, operator) {    
+  return operator(num1, num2)
+}
+console.log(calculator(2, 3, multiply)); // asking the calculator to multiply 2 and 3 using the multiply function.
+
+
+
+
 let selector = document.querySelector("button");
 
 function randomNumber(num) {
