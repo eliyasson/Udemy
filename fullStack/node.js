81fbbf5456cd72cefe58c3d4583ef0019915d8f6
eliyas <eliyasson11@gmail.com>
2023-11-22 15:50:21 +0200

    nodeJs

--- fullStack/node.js
+++ fullStack/node.js
@@ -1,9 +1,17 @@
 const fs = require("fs");
 
-fs.writeFile("message.txt", "Hello from NodeJS!", (err) => {
+/* const filePath = "message.txt";
+const contentToWrite = "Hello"; */
+
+/* fs.writeFile(filePath, contentToWrite, (err) => {
     if (err) {
         console.error("Error occurred while writing to file:", err);
     } else {
-    console.log("The file has been saved!");
+        console.log("The file has been saved!");
     }
+});    */ 
+fs.readFile("message.txt", "utf8", (err, data) => {
+    if (err) throw err;
+        
+    console.log(data);
 });
