bcaa825f4dd631bfe8cd4f8dd52548be1a75c496
eliyas <eliyasson11@gmail.com>
2023-11-24 23:20:12 +0200

    commit

--- fullStack/node.js
+++ fullStack/node.js
@@ -1,15 +1,15 @@
 const fs = require("fs");
 
-/* const filePath = "message.txt";
-const contentToWrite = "Hello"; */
+const filePath = "message.txt";
+const contentToWrite = "Hello";
 
-/* fs.writeFile(filePath, contentToWrite, (err) => {
+fs.writeFile(filePath, contentToWrite, (err) => {
     if (err) {
         console.error("Error occurred while writing to file:", err);
     } else {
         console.log("The file has been saved!");
     }
-});    */ 
+});    
 fs.readFile("message.txt", "utf8", (err, data) => {
     if (err) throw err;
         
