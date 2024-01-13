661dbe9c77516e5c5e4e06909f32a6a6e152f20b
eliyas <eliyasson11@gmail.com>
2024-01-13 11:43:54 +0200

    JSX

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -5,8 +5,8 @@ const name = 'Eliyas';
 const randNum = Math.floor(Math.random() * 10);
 
 ReactDOM.render(
-  <div>
+  <>
     <h3>Hey {name}! </h3>
     <p>Your lucky number is {randNum}</p>
-  </div>, 
+  </>, 
     document.getElementById("root"));
