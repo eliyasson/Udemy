20df25a510911fa07dcd428ed547d76fbff74a38
eliyas <eliyasson11@gmail.com>
2024-01-14 12:41:23 +0200

    JSX element practice

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name = 'Eliyas';
-const randNum = Math.floor(Math.random() * 10);
+/* const name = 'Eliyas';
+const randNum = Math.floor(Math.random() * 10); */
 
 ReactDOM.render(
   <>
-    <h3>Hey {name}! </h3>
-    <p>Your lucky number is {randNum}</p>
+    <h1>Shopping items</h1>
+    <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and 
+      publishing industries for previewing layouts and visual mockups.</p>
+    <ol>
+      <li>Rice</li>
+      <li>Tea powder</li>
+      <li>Sugar</li>
+      <li>Oil</li>
+      <li>Wheat</li>
+    </ol>
   </>, 
     document.getElementById("root"));
