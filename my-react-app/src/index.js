c03b5b28768e0042b86df08fc8f4dc4a9091c00d
eliyas <eliyasson11@gmail.com>
2024-01-16 00:39:08 +0200

    commit

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-/* const name = 'Eliyas';
-const randNum = Math.floor(Math.random() * 10); */
-
+const name = "Eliyas";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
 ReactDOM.render(
   <>
-    <h1>Shopping items</h1>
-    <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and 
-      publishing industries for previewing layouts and visual mockups.</p>
-    <ol>
-      <li>Rice</li>
-      <li>Tea powder</li>
-      <li>Sugar</li>
-      <li>Oil</li>
-      <li>Wheat</li>
-    </ol>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
+    
   </>, 
     document.getElementById("root"));
