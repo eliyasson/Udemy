8fcd3f5739ecb3f1569f48821c7385bae1e9b88a
eliyas <eliyasson11@gmail.com>
2024-01-17 11:38:00 +0200

    inline style

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 const name = "Eliyas";
 const currentDate = new Date();
 const year = currentDate.getFullYear();
+
+const customeStyle = {
+  color: "red",
+  fontsize: "20px",
+  border: "1px solid black"
+};
+
 ReactDOM.render(
   <>
-    <p>Created by {name}</p>
+    <p style={customeStyle}>Created by {name}</p>
     <p>Copyright {year}</p>
     
   </>, 
