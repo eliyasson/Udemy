10bd8a75f9c94479513437b2324c185191ef0fac
eliyas <eliyasson11@gmail.com>
2024-01-17 14:39:37 +0200

    creatin components

---
+++ my-react-app/src/List.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function List() {
+    <ul>
+      <li>Bacon</li>
+      <li>Pasta</li>
+      <li>Noodles</li>
+    </ul>
+}
+export default List;
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-const date = new Date();
-const year = date.getFullYear();
-const time = date.getHours();
-
-let greatings;
-if (time < 12) {
-  greatings = 'Good Morning';
-} else if (time >= 12  && time < 17 ) {
-  greatings = 'Good Afternoon';
-} else {
-  greatings = 'Good Evening';
-}
+import Heading from './Heading';
+import List from './List';
 
 ReactDOM.render(
   <>
-    <p>{greatings}</p>
-    <p>current date: {year}</p>
-    <p>current time: {time}</p>
+    <Heading />
+    <List />
   </>
 ,document.getElementById("root"));
