107db29af3c93522f9daf661364645b95cd7d943
eliyas <eliyasson11@gmail.com>
2024-03-09 01:52:14 +0200

    practicing

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -2,11 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import {addition, subtraction} from './calculator';
+function Card(props) {
+  return (
+  <div>
+    <h2>{props.name}</h2>
+    <img
+      src={props.img}
+      alt="avatar_img"
+    />
+    <p>{props.tel}</p>
+    <p>{props.email}</p>
+  </div>
+);
+}
+
+function Contact(props) {
+  return ( 
+    <div>
+      <h3>{props.name}</h3>
+
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <ul>
-    <li>{addition(99, 9)}</li>
-    <li>{subtraction(99, 9)}</li>
-  </ul>
-,document.getElementById("root"));
+  <div>
+    <h1>My Contacts</h1>
+    <Card 
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      tel="+123 4567 909"
+      email="b@gmail.com"
+    />
+    <Contact 
+      name="Ruth"/>
+  </div>,
+
+  document.getElementById("root")
+);
