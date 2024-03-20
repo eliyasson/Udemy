fda5da90bb513f296f8731ce4f0da170185b51b9
eliyas <eliyasson11@gmail.com>
2024-03-21 01:05:04 +0200

    rendering

--- my-react-app/src/component/Login.jsx
+++ my-react-app/src/component/Login.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import Input from "./Input";
 
-const isLoggedIn = false ? renderConditionally() : loginForm();
+const currentTime = new Date().getHours();
 
-function renderConditionally() {
-    return <h1>Hello</h1>
-} 
-function loginForm() {
-    return <Input />
+function Greatings() {
+    return currentTime < 12 ? <h2>Good Morning!</h2> : <h2>Good afternoon!</h2>
 }
 
+function registerForm() {
+    return (
+        <form className="form">
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Confirm Password" />
+          <button type="submit">Register</button>
+        </form>
+      );
+}
+
+const isLoggedIn = false ? <h1>{registerForm()}</h1> :  <Input />;
 function Login() {
     return (
         <div className="container">
+            {Greatings()}
             {isLoggedIn} 
         </div>
     )
