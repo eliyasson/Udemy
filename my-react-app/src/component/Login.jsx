8a69dc08af88c776fa40c198895708ec039d3b40
eliyas <eliyasson11@gmail.com>
2024-03-26 00:00:50 +0200

    imperative

--- my-react-app/src/component/Login.jsx
+++ my-react-app/src/component/Login.jsx
@@ -6,11 +6,21 @@ function Greatings() {
     return currentTime < 12 ? <h2>Good Morning!</h2> : <h2>Good afternoon!</h2>
 }
 
-const isLoggedIn = true;
+function strike() {
+    document.getElementById("root").style.textDecoration = "line-through"
+}
+function unStrike() {
+    document.getElementById("root").style.textDecoration = null;
+}
 
 function Login() {
+    const isLoggedIn = true;
+
     return (
         <div className="container">
+            <p>Buy house</p>
+            <button onClick={strike}>Change to strike through</button>
+            <button onClick={unStrike}>Change back</button>
             <h2>{Greatings()}</h2>
             <form className="form">
                 <input type="text" placeholder="Username" />
