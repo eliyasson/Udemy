2d5f4e5b55664bb6bc967f5cc0c01ac0cc62215e
eliyas <eliyasson11@gmail.com>
2024-03-28 15:44:34 +0200

    Hooks

--- my-react-app/src/component/Login.jsx
+++ my-react-app/src/component/Login.jsx
@@ -1,34 +1,21 @@
 import React, { useState } from "react";
 import { ReactDOM } from "react";
 
-
-const currentTime = new Date().getHours();
-
-
-
-
-function Greatings() {
-    return currentTime < 12 ? <h2>Good Morning!</h2> : <h2>Good afternoon!</h2>
-}
-
-
+const currentTime = new Date().toLocaleTimeString();
 function Login() {
-    const [count, setCount]  = useState(0);
-    const isLoggedIn = true;
+    const [time, setTime] = useState("");
 
-    function increament() {
-        setCount(count + 1);
+    function getTime() {
+        setTime(currentTime);
     }
     return (
         <div className="container">
-            <h1>{count}</h1>
-            <button onClick={increament}>+</button>
-            <h2>{Greatings()}</h2>
+            <h2>{time}</h2>
+            <button onClick={getTime}>Get Time</button>
             <form className="form">
                 <input type="text" placeholder="Username" />
                 <input type="password" placeholder="Password" />
-                {!isLoggedIn && <input type="password" placeholder="Confirm Password" />}
-                <button type="submit">{isLoggedIn  ? "Login" :  "Register" }</button>
+                <button type="submit">Login</button>
             </form>
         </div>
     )
