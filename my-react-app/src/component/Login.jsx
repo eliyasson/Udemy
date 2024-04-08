5e7e21f492866e226da0699244e5fcfd74020e47
eliyas <eliyasson11@gmail.com>
2024-04-08 17:32:41 +0300

    complex state

--- my-react-app/src/component/Login.jsx
+++ my-react-app/src/component/Login.jsx
@@ -5,31 +5,48 @@ function Login() {
     
     const [fullName, setFullName] = useState({
         fname: "",
-        lname: ""
+        lname: "",
+        email:""
     });
 
-
     function handleChange(event) {
-        const newValue = event.target.value;
-        const inputName = event.target.name;
-
-        if (inputName === "fname") {
-            setFullName({ fname: newValue})
-        } else if ( inputName === "lname") {
-            setFullName( { lname: newValue});
-        }
-    }
+        const {value, name} = event.target;
+        
  
+
+        setFullName(prevValue => {
+           if (name === "fname") {
+            return {
+                fname: value,
+                lname: prevValue.lname,
+                email: prevValue.email
+            }
+           } else if(name === "lname") {
+            return {
+                fname: prevValue.fname,
+                lname: value,
+                email: prevValue.email
+            }
+           } else if(name === "email") {
+            return {
+                email: value,
+                lname: prevValue.lname,
+                fname: prevValue.email
+            }
+           }
+        })
+    }
 
     return (
         <div className="container">
             <h1>Todo List</h1>
             <h2>Hello, {fullName.fname} {fullName.lname}</h2>
+            <h3>{fullName.email}</h3>
             <form  className="form">
                 <input 
                     name="fname" 
                     onChange={handleChange}  type="text" 
-                    placeholder="First Name" 
+                    placeholder="First Name"  
                     value={fullName.fname}
                 />
 
@@ -38,6 +55,12 @@ function Login() {
                     name="lname" type="text" 
                     placeholder="Last Name" 
                     value={fullName.lname}
+                />
+                <input 
+                    onChange={handleChange} 
+                    name="email" type="text" 
+                    placeholder="Email" 
+                    value={fullName.email}
                 />
 
                 <button type="submit">Submit</button>
